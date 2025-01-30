53aa5d30c4f5d44a8e32ec158e3c442d9fa6b418
Suwagath S. <suwagath.srithar@gmail.com>
2025-01-31 02:26:39 +0530

    Modify to include the ScheduleSummaryPage

--- MindAnchor-FrontEnd/src/routes/+page.svelte
+++ MindAnchor-FrontEnd/src/routes/+page.svelte
@@ -1,18 +1,21 @@
 <script>
-  import { currentPage } from '../store.js';
-  import WelcomePage from '../components/WelcomePage.svelte';
-  import Dashboard from '../components/Dashboard.svelte'; // Add this component later
-	import SchedulePage from '../components/SchedulePage.svelte';
-	import BlackListWhiteListPage from '../components/BlackList_WhiteListPage.svelte';
-
-</script>
-
-{#if $currentPage === 'WelcomePage'}
-    <WelcomePage />
-{:else if $currentPage === 'Dashboard'}
-    <Dashboard />
-{:else if $currentPage === 'SchedulePage'}
-    <SchedulePage />
-{:else if $currentPage === 'BlackList_WhiteListPage'}
-    <BlackListWhiteListPage />
-{/if}
+    import { currentPage } from '../store.js';
+    import WelcomePage from '../components/WelcomePage.svelte';
+    import Dashboard from '../components/Dashboard.svelte';
+    import SchedulePage from '../components/SchedulePage.svelte';
+    import BlackListWhiteListPage from '../components/BlackList_WhiteListPage.svelte';
+    import ScheduleSummaryPage from '../components/ScheduleSummaryPage.svelte';
+  </script>
+  
+  {#if $currentPage === 'WelcomePage'}
+      <WelcomePage />
+  {:else if $currentPage === 'Dashboard'}
+      <Dashboard />
+  {:else if $currentPage === 'SchedulePage'}
+      <SchedulePage />
+  {:else if $currentPage === 'BlackList_WhiteListPage'}
+      <BlackListWhiteListPage />
+  {:else if $currentPage === 'ScheduleSummaryPage'}
+      <ScheduleSummaryPage />
+  {/if}
+  
