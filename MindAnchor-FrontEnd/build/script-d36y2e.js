8da03b50208bbc09c1298f3acd1edfc0193b0c29
Rishan <rishanmuyeen116@gmail.com>
2025-02-02 15:22:25 +0530

    Build folder

---
+++ MindAnchor-FrontEnd/build/script-d36y2e.js
@@ -0,0 +1,23 @@
+
+				{
+					__sveltekit_vr4sb5 = {
+						base: new URL(".", location).pathname.slice(0, -1)
+					};
+
+					const element = document.currentScript.parentElement;
+
+					const data = [null,null];
+
+					Promise.all([
+						import("./app/immutable/entry/start.jSaaYsF_.js"),
+						import("./app/immutable/entry/app.Og6AGNyX.js")
+					]).then(([kit, app]) => {
+						kit.start(app, element, {
+							node_ids: [0, 2],
+							data,
+							form: null,
+							error: null
+						});
+					});
+				}
+			
