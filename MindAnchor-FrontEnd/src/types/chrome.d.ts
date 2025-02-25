cbbeb312a949268521812e86ebfe971455aa0b61
Suwagath S. <suwagath.srithar@gmail.com>
2025-02-25 16:13:55 +0530

    declare chrome API explicitly

---
+++ MindAnchor-FrontEnd/src/types/chrome.d.ts
@@ -0,0 +1,10 @@
+declare namespace chrome {
+  export namespace storage {
+    export const local: {
+      get(key: string, callback: (data: any) => void): void;
+    };
+  }
+  export namespace runtime {
+    export function sendMessage(message: any, callback?: () => void): void;
+  }
+} 
