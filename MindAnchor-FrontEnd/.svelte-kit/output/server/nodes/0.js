9ae8c20c540c9178d1970e3cd827f6ddc8db2f7d
Rishan <rishanmuyeen116@gmail.com>
2025-02-28 21:35:52 +0530

    created a navigation template in user progress to access other pages

--- MindAnchor-FrontEnd/.svelte-kit/output/server/nodes/0.js
+++ MindAnchor-FrontEnd/.svelte-kit/output/server/nodes/0.js
@@ -1,10 +1,8 @@
 import * as universal from '../entries/pages/_layout.ts.js';
 
 export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
 export { universal };
 export const universal_id = "src/routes/+layout.ts";
-export const imports = ["app/immutable/nodes/0.BZ6BuNKf.js","app/immutable/chunks/BgcssLAV.js","app/immutable/chunks/DfSOqxX-.js"];
-export const stylesheets = ["app/immutable/assets/0.CILR4D9e.css"];
+export const imports = [];
+export const stylesheets = [];
 export const fonts = [];
--- MindAnchor-FrontEnd/.svelte-kit/output/server/nodes/2.js
+++ MindAnchor-FrontEnd/.svelte-kit/output/server/nodes/2.js
@@ -1,8 +1,6 @@
 
 
 export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["app/immutable/nodes/2.DKeQsUlo.js","app/immutable/chunks/BgcssLAV.js","app/immutable/chunks/DfSOqxX-.js","app/immutable/chunks/CWu7DY8o.js","app/immutable/chunks/DB89Dqc7.js","app/immutable/chunks/D8WOyEKT.js","app/immutable/chunks/FDJc48Q4.js"];
-export const stylesheets = ["app/immutable/assets/2.D4yJl7Qo.css"];
+export const imports = [];
+export const stylesheets = [];
 export const fonts = [];
