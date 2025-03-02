a606a9bd579d2c060a58e58878a9777a46b9b58f
Rafan <rafanliya@gmail.com>
2025-03-02 17:37:03 +0530

    Removed previous progress data integration attempt

--- MindAnchor-FrontEnd/build/script-8rcgn1.js
+++ MindAnchor-FrontEnd/build/script-8rcgn1.js
@@ -1,14 +1,14 @@
 
 				{
-					__sveltekit_igoc41 = {
+					__sveltekit_ohehi2 = {
 						base: new URL(".", location).pathname.slice(0, -1)
 					};
 
 					const element = document.currentScript.parentElement;
 
 					Promise.all([
-						import("./app/immutable/entry/start.Av9_kuY-.js"),
-						import("./app/immutable/entry/app.BG_ul6aP.js")
+						import("./app/immutable/entry/start.DlIomaDy.js"),
+						import("./app/immutable/entry/app.YchquobW.js")
 					]).then(([kit, app]) => {
 						kit.start(app, element, {
 							node_ids: [0, 2],
