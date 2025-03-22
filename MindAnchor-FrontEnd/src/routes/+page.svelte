7e8e00133017e3c5267c9a83fdc62e5a775fa91b
Rishan <rishanmuyeen116@gmail.com>
2025-03-22 09:38:04 +0530

    Updated the page.svelte

--- MindAnchor-FrontEnd/src/routes/+page.svelte
+++ MindAnchor-FrontEnd/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
     import WelcomePage from './Welcome_Page/WelcomePage.svelte';
-    import Dashboard from './DashBoard_Page/Dashboard.svelte';
-    import SchedulePage from './Schedule_Page/SchedulePage.svelte';
     import BlackListWhiteListPage from './BlackList_WhiteList_Page/BlackList_WhiteListPage.svelte';
-    import ScheduleSummaryPage from './Schedule_Summary_Page/ScheduleSummaryPage.svelte';
     import T_and_C_Page from './T_and_C_Page/T_and_C_Page.svelte';
 	import { currentPage } from '../lib/store';
 	import ActiveStatePage from './Active_State/Active_State_Page.svelte';
@@ -14,14 +11,8 @@
   
 {#if $currentPage === 'WelcomePage'}
     <WelcomePage />
-{:else if $currentPage === 'Dashboard'}
-    <Dashboard />
-{:else if $currentPage === 'SchedulePage'}
-    <SchedulePage />
 {:else if $currentPage === 'BlackList_WhiteListPage'}
     <BlackListWhiteListPage />
-{:else if $currentPage === 'ScheduleSummaryPage'}
-    <ScheduleSummaryPage />
 {:else if $currentPage === 'T_and_C_Page'}
     <T_and_C_Page />
 {:else if $currentPage === 'ProgressChart'}
