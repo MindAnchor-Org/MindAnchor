a94104913beebfd4013c1d1c4d04b627a31da5d0
Rishan <rishanmuyeen116@gmail.com>
2025-03-22 21:42:05 +0530

    Fixed the bg color and text color
    Saved the bg and text color from the settings page and saved it to the chrome storage, also tried to work on the image!! couldnt fix

--- MindAnchor-FrontEnd/src/routes/Active_State/Active_State_Page.svelte
+++ MindAnchor-FrontEnd/src/routes/Active_State/Active_State_Page.svelte
@@ -211,22 +211,6 @@ async function showMotivationalCue() {
       }
     })
     goToBlackList_WhiteListPage();
-    chrome.storage.local.set({ ActivateClassification: 'false' }, function () {
-      if (chrome.runtime.lastError) {
-        console.error("Error saving data:", chrome.runtime.lastError);
-      } else {
-        console.log("Data saved successfully from activestate page");
-
-        // Retrieving the value immediately after saving
-        chrome.storage.local.get(['ActivateClassification'], function (result) {
-          if (chrome.runtime.lastError) {
-            console.error("Error getting value:", chrome.runtime.lastError);
-          } else {
-            console.log("Value retrieved from active statepage:", result.ActivateClassification);
-          }
-        });
-      }
-    });
   }
 
   function goToBlackList_WhiteListPage() {
